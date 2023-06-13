6e51f0bac3d0e661a51be093d469dcc4ef619874
nadiellymedeiros <nadiellymedeiros@gmail.com>
2023-06-13 16:00:41 -0300

    new navbar and new pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,101 +3,203 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import Article from "@mui/icons-material/Article";
+import Link from "next/link";
 
-export default function MyMenu() {
-  // const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-  //   null
-  // );
-  // const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-  //   null
-  // );
+interface Props {
+  window?: () => Window;
+}
 
-  // const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElNav(event.currentTarget);
-  // };
-  // const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
 
-  // const handleCloseNavMenu = () => {
-  //   setAnchorElNav(null);
-  // };
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        News Tech Pro bem
+      </Typography>
+      <Divider />
+
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <>
-      <AppBar position="static" sx={{ bgcolor: "#01579b" }}>
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Article
-              sx={{
-                display: { xs: "none", md: "flex" },
-                mr: 1,
-              }}
-            />
+    <Box sx={{ display: "flex", textDecoration: "none" }}>
+      <CssBaseline />
+      <AppBar component="nav" position="static" sx={{ bgcolor: "#01579b" }}>
+        <Toolbar>
+          <Article
+            sx={{
+              display: { xs: "none", md: "flex" },
+              mr: 1,
+            }}
+          />
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".1rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              News Tech Pro bem
-            </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".1rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            News Tech Pro bem
+          </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                // onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-            </Box>
-            <Article sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          {/* <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "#fff" }}>
+                {item}
+              </Button>
+            ))}
+          </Box> */}
+
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                md: "flex",
+              },
+            }}
+          >
             <Typography
               variant="h5"
               noWrap
               component="a"
-              href=""
+              href="/"
               sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
                 textDecoration: "none",
-                fontSize: 12,
+                mr: 2,
+                display: { xs: "none", sm: "block", fontSize: 14 },
               }}
             >
-              News Tech Pro Bem
+              Home
             </Typography>
-          </Toolbar>
-        </Container>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/about"
+              sx={{
+                textDecoration: "none",
+                mr: 2,
+                display: { xs: "none", sm: "block", fontSize: 14 },
+              }}
+            >
+              About
+            </Typography>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/contact"
+              sx={{
+                textDecoration: "none",
+                mr: 2,
+                display: { xs: "none", sm: "block", fontSize: 14 },
+              }}
+            >
+              Contact
+            </Typography>
+          </Box>
+
+          <Article
+            sx={{
+              display: { xs: "flex", md: "none" },
+              mr: 1,
+            }}
+          />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+
+              fontSize: 12,
+            }}
+          >
+            News Tech Pro Bem
+          </Typography>
+        </Toolbar>
       </AppBar>
-    </>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
   );
 }
---
+++ src/pages/about.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/Navbar";
+import Box from "@mui/material/Box";
+
+export default function about() {
+  return (
+    <>
+      <Navbar />
+      <Box
+        sx={{
+          mr: 5,
+          p: 5,
+          textAlign: "center",
+          justifyContent: "center",
+          fontSize: 20,
+          fontWeight: 500,
+        }}
+      >
+        Project created to consume the newsapi api.
+      </Box>
+    </>
+  );
+}
